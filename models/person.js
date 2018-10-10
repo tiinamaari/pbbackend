0220aee8280295de223d5018e5fb5c7cf2834b4e
tiinamaari <43540047+tiinamaari@users.noreply.github.com>
2018-10-10 15:49:36 +0300

    Update person.js

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://tinde:passu123@ds231315.mlab.com:31315/amazingphonebook'
+const url = 'mongodb://<username>:<password>@ds231315.mlab.com:31315/amazingphonebook'
 
 mongoose.connect(url)
 
